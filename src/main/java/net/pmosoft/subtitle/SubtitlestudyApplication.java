7058681905c4cb5f1dd1955441f574d71d2f62d8
lifedomy <lifedomy@gmail.com>
2018-09-21 00:33:21 +0900

    ..

--- src/main/java/net/pmosoft/subtitle/SubtitlestudyApplication.java
+++ src/main/java/net/pmosoft/subtitle/SubtitlestudyApplication.java
@@ -2,11 +2,34 @@ package net.pmosoft.subtitle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class SubtitlestudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubtitlestudyApplication.class, args);
+	}
+	
+	@Configuration
+	public class WebApplicationConfig extends WebMvcConfigurerAdapter {
+
+	    @Override
+	    public void addViewControllers(ViewControllerRegistry registry) {
+	        registry.addViewController("/notFound").setViewName("forward:/index.html");
+	    }
+
+	    @Bean
+	    public EmbeddedServletContainerCustomizer containerCustomizer() {
+	        return container -> {
+	            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,"/notFound"));
+	        };
+	    }
 	}
 }
